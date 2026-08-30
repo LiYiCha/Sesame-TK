7c3f6b7fed3e6e0955744c2eaa4a0746eef79bbc
yicha <3258163979@qq.com>
2026-08-30 11:10:44 +0800

    fix: 更新花呗金签到处理和升版0.4.2

--- app/src/main/java/fansirsqi/xposed/sesame/task/otherTask/HuaBeiJin.java
+++ app/src/main/java/fansirsqi/xposed/sesame/task/otherTask/HuaBeiJin.java
@@ -4,76 +4,89 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import fansirsqi.xposed.sesame.data.Status;
 import fansirsqi.xposed.sesame.hook.RequestManager;
-import fansirsqi.xposed.sesame.util.JsonUtil;
 import fansirsqi.xposed.sesame.util.Log;
 import fansirsqi.xposed.sesame.util.RandomUtil;
 import fansirsqi.xposed.sesame.util.TimeUtil;
 
 public class HuaBeiJin extends BaseCommTask {
-    private final String productCode = "";
-    private final String sceneCode = "NUGGETS_HUABEI_GOLDEN_POINT";
 
     public HuaBeiJin() {
         this.displayName = "花呗金💴";
-        //this.hoursKeyEnum = CompletedKeyEnum.HuaBeiJin;
     }
 
     @Override
     protected void handle() {
-        queryV2();
+        signIn();
     }
 
-    private void queryV2() {
-        TimeUtil.sleep(RandomUtil.nextInt(1000,3000));
-        JSONObject requestString2;
-        JSONArray jSONArray;
+    private void signIn() {
+        if(Status.hasFlagToday("HuaBeiJin:Sign")) return;
+
+        TimeUtil.sleep(RandomUtil.nextInt(1000, 2000));
         try {
-            String res = RequestManager.requestString("com.alipay.pcreditbfweb.sdk.task.queryV2",
-                    "[{\"requestFrom\": \"pccp\",\"scene\":\"NUGGETS_HUABEI_GOLDEN_POINT\"}]");
-            JSONObject requestString = new JSONObject(res);
-            if (requestString != null|| !requestString.getBoolean("success")) {
-                JSONArray jSONArray2 = requestString.getJSONArray("data");
-                JSONArray jSONArray3 = new JSONArray();
-                JSONArray jSONArray4 = new JSONArray();
-                for (int i = 0; i < jSONArray2.length(); i++) {
-                    JSONObject jSONObject = jSONArray2.getJSONObject(i);
-                    String string = jSONObject.getString("taskId");
-                    String string2 = jSONObject.getString("taskCenId");
-                    jSONObject.getString("taskStatus");
-                    if (!"CLICK_JUMP".equals(jSONObject.getString("taskTriggerType"))) {
-                        String valueByPath = JsonUtil.getValueByPath(jSONObject, "taskShowInfo.title");
-                        trigger(string2, string, "signup");
-                        if (trigger(string2, string, "send")) {
-                            Log.other(this.displayName + "完成[" + valueByPath + "]");
-                            jSONArray3.put(string2);
-                            jSONArray4.put(string);
-                        }
+            String queryRes = RequestManager.requestString(
+                    "com.alipay.pcreditrecweb.needle.hbjQuerySignInfo",
+                    "[{}]"
+            );
+            if (queryRes.isEmpty()) {
+                Log.error("花呗金--查询签到信息响应为空");
+                return;
+            }
+            JSONObject queryJo = new JSONObject(queryRes);
+            if (!queryJo.optBoolean("success", false)) {
+                Log.error("花呗金--查询签到信息失败: " + queryJo.optString("errorMsg", queryRes));
+                return;
+            }
+            JSONObject result = queryJo.optJSONObject("result");
+            if (result == null) {
+                Log.error("花呗金--签到数据结构异常");
+                return;
+            }
+            if (!result.optBoolean("accountExists", true) || result.optBoolean("notAllow", false)) {
+                Log.other(this.displayName + "未开通花呗或暂不支持签到");
+                return;
+            }
+            if (result.optBoolean("todaySignIn", false)) {
+                Log.other(this.displayName + "今日已签到");
+                return;
+            }
+
+            String playId = result.optString("playId", "");
+            if (playId.isEmpty()) {
+                Log.error("花呗金--未获取到 playId");
+                return;
+            }
+
+            TimeUtil.sleep(RandomUtil.nextInt(1500, 3000));
+            String signInRes = RequestManager.requestString(
+                    "com.alipay.pcreditrecweb.needle.hbjSignIn",
+                    "[{\"playId\":\"" + playId + "\"}]"
+            );
+            if (signInRes.isEmpty()) {
+                Log.error("花呗金--签到响应为空");
+                return;
+            }
+            JSONObject signInJo = new JSONObject(signInRes);
+            if (signInJo.optBoolean("success", false) || "SUCCESS".equals(signInJo.optString("errorCode"))) {
+                JSONObject signInResult = signInJo.optJSONObject("result");
+                int price = 1;
+                if (signInResult != null) {
+                    JSONArray rewardInfos = signInResult.optJSONArray("rewardInfos");
+                    if (rewardInfos != null && rewardInfos.length() > 0) {
+                        price = rewardInfos.getJSONObject(0).optInt("price", 1);
                     }
-                    TimeUtil.sleep(RandomUtil.nextInt(15000,20000));
                 }
-                if (jSONArray3.length() != 0 && (requestString2 = requestString("com.alipay.pcreditbfweb.sdk.task.award", "\"taskCenIds\":" + jSONArray3 + ",\"taskIds\":" + jSONArray4)) != null && (jSONArray = (JSONArray) JsonUtil.getValueByPathObject(requestString2, "data.resultData")) != null) {
-                    for (int i2 = 0; i2 < jSONArray.length(); i2++) {
-                        Log.other(this.displayName + "获得[" + JsonUtil.getValueByPath(jSONArray.getJSONObject(i2), "prizeSendOrderList.[0].price") + "花呗金]");
-                    }
-                }
+                Log.other(this.displayName + "签到成功获得[" + price + "花呗金]");
+            } else {
+                Log.other(this.displayName + "签到失败: " + signInJo.optString("errorMsg", "未知错误"));
             }
         } catch (JSONException e) {
-            Log.error("dubug--花呗金JSON解析异常:"+e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private boolean trigger(String str, String str2, String str3) {
-        try {
-            return requestString("com.alipay.pcreditbfweb.sdk.task.trigger", new StringBuilder("\"appletId\": \"").append(str2).append("\",\"outBizNo\": \"").append(str2).append(TimeUtil.getMinuteTimestamp()).append("\",\"taskCenId\": \"").append(str).append("\",\"retryFlag\": true,\"stageCode\":\"").append(str3).append("\"").toString()) != null;
+            Log.error("花呗金JSON解析异常: " + e);
         } catch (Throwable th) {
-            try {
-                Log.printStackTrace(this.TAG, th);
-                return false;
-            } finally {
-                TimeUtil.sleep(this.executeIntervalInt);
-            }
+            Log.error(this.TAG, "花呗金异常："+th);
         }
+        Status.setFlagToday("HuaBeiJin:Sign");
     }
 }
