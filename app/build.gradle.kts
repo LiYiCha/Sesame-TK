22765d3c7073ede649ed8d1eab9e215f531e7b96
yicha <3258163979@qq.com>
2026-09-05 15:58:28 +0800

    feat: 0.4.2版本
    - 新增花呗金签到逻辑。
    -优化资源处理。
    -新增芝麻炼金的体力和开宝箱任务

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ android {
         }
 
 
-        val versionNumber = "0.4.2"
+        val versionNumber = "0.4.3"
         val dateString = SimpleDateFormat("yyMMdd", Locale.CHINA).apply {
             timeZone = TimeZone.getTimeZone("GMT+8")
         }.format(Date())
