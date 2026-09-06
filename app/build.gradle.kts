f0af112f219f1d1296731d0c7f34ce3e17077031
yicha <3258163979@qq.com>
2026-09-06 22:42:10 +0800

    release: 升级至 v0.4.8，支持 Markdown 版本说明与客户端内置附加包管理发布

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,11 +67,11 @@ android {
         }
 
 
-        val versionNumber = "0.4.7"
+        val versionNumber = "0.4.8"
         val dateString = SimpleDateFormat("yyMMdd", Locale.CHINA).apply {
             timeZone = TimeZone.getTimeZone("GMT+8")
         }.format(Date())
-        versionCode = 32
+        versionCode = 33
         val buildTag = "beta"
         versionName = "$versionNumber-$dateString"
 
